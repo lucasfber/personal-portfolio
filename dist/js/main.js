07dcf9e91203e4f939f0f456a752e38eed4c5a69
Lucas Bertoldo <lucas.fber@gmail.com>
2020-02-07 00:21:23 -0300

    About-Me section - DESKTOP VERSION done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,18 @@
 let btnMenu = document.querySelector(".btn-menu")
 let menu = document.querySelector(".menu")
+let navLink = document.querySelector(".nav-links__link")
 
 btnMenu.addEventListener("click", function() {
-  this.classList.toggle("close")
-  menu.classList.toggle("open")
+  toggleMenu(this, menu)
 })
+
+function toggleMenu(btnMenu, menu) {
+  btnMenu.classList.toggle("close")
+  menu.classList.toggle("open")
+}
+
+navLink.addEventListener("click", function() {
+  toggleMenu(btnMenu, menu)
+})
+
+//function disposeMenu() {}
